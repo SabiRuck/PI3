b50b589d1a268c65ca6166bd790e124b15ab4228
Sabi Ruckova <sabinka.ruckova@gmail.com>
2025-12-10 09:25:08 +0100

    blblblblb migrations

--- prvy/skuska/admin.py
+++ prvy/skuska/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Osoba, Student
 
 # Register your models here.
+admin.site.register(Osoba)
+admin.site.register(Student)
--- prvy/skuska/models.py
+++ prvy/skuska/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Osoba(models.Model):
+    meno = models.CharField(max_length=100)
+    priezvisko = models.CharField(max_length=100)
+    vek = models.IntegerField()
+    datum = models.DateField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.meno} {self.priezvisko}"
+    
+
+class Student(models.Model):
+    meno = models.CharField(max_length=100)
+    priezvisko = models.CharField(max_length=100)
+    trieda = models.CharField(max_length=10)
+    vek = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.meno} {self.priezvisko} {self.trieda}"
